37ffd5ac831a19ad320d38943bddca1f31529971
Tom Dyer <tdyer1@gmail.com>
2015-11-03 10:40:46 -0500

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601012338) do
+ActiveRecord::Schema.define(version: 20151103153855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 20150601012338) do
   end
 
   add_index "songs", ["album_id"], name: "index_songs_on_album_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "reviews", "movies"
   add_foreign_key "songs", "albums"
